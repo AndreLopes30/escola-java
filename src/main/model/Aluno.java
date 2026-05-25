1d31a455316d5d69da8440fc83bdf894d4b060f2
AndreLopes30 <ferreiraandre833@gmail.com>
2026-05-25 16:19:31 -0300

    first commit

---
+++ src/main/model/Aluno.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Aluno {
+    private String nomme;
+    private double nota;
+    private int idade;
+    private char turma;
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    public String getNomme() {
+        return nomme;
+    }
+
+    public void setNomme(String nomme) {
+        this.nomme = nomme;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public char getTurma() {
+        return turma;
+    }
+
+    public void setTurma(char turma) {
+        this.turma = turma;
+    }
+}
