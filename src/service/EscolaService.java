2f53e94544b17f0a787aeae4e3f15fed4d10c9dd
AndreLopes30 <ferreiraandre833@gmail.com>
2026-05-25 20:37:10 -0300

    Criando service

--- src/service/EscolaService.java
+++ src/service/EscolaService.java
@@ -1,33 +1,47 @@
 package service;
 
 import model.Aluno;
-import model.AlunoBolsista;
+
 import java.util.ArrayList;
 
 public class EscolaService {
-    ArrayList<Aluno> alunos = new ArrayList<>();
+    ArrayList<Aluno> alunosLista = new ArrayList<>();
     public Aluno adicionar() {
 
     }
     public Aluno listar() {
-
+        for(Aluno aluno : alunosLista) {
+            return aluno;
+        }
     }
     public Aluno listar_aprovados() {
-
+        for(Aluno aluno : alunosLista) {
+            if(aluno.getNota() >= 6){
+                return aluno;
+            }
+        }
     }
     public Aluno listar_reprovados() {
-
+        for(Aluno aluno : alunosLista) {
+            if(aluno.getNota() < 6){
+                return aluno;
+            }
+        }
     }
-    public Aluno buscar_aluno() {
-
+    public Aluno buscar_aluno(String busca) {
+        for(Aluno aluno : alunosLista) {
+            if(aluno.getNome().equals(busca)){
+                return aluno;
+            }
+        }
     }
 
     public double exibir_média() {
         double nota = 0;
-        for(Aluno aluno : alunos) {
+        for(Aluno aluno : alunosLista) {
             nota += aluno.getNota();
         }
-        return nota/alunos.toArray().length;
+        return nota/ alunosLista.toArray().length;
     }
 
 }
